e3e7d983da2376be29cc4a33ff536cfa2c67617b
Jeremy ALONSO <jealonso@e3r12p8.42.fr>
2015-06-10 18:29:17 +0200

    fix ligne blanche avant permission denied

--- srcs/error.c
+++ srcs/error.c
@@ -6,7 +6,7 @@
 /*   By: jealonso <jealonso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/05 15:36:07 by jealonso          #+#    #+#             */
-/*   Updated: 2015/06/10 18:21:37 by jealonso         ###   ########.fr       */
+/*   Updated: 2015/06/10 18:26:04 by jealonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	ft_error(char *name)
 
 void	ft_bad_permission(char *name)
 {
-	ft_putstr_fd("\nft_ls: ", 2);
+	ft_putstr_fd("ft_ls: ", 2);
 	ft_putstr_fd(name, 2);
 	ft_putendl_fd(": Permission denied", 2);
 }
